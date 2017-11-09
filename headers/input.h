5483c70c4382f4a65c7e680c903c625853161d2d
pixelherodev <pixelherodev@outlook.com>
2017-11-09 10:49:55 -0500

    Add more symbols and the uppercase letters

--- headers/input.h
+++ headers/input.h
@@ -5,7 +5,11 @@ enum keycode_t{
 	keyq,keyw,keye,keyr,keyt,keyy,keyu,keyi,keyo,keyp,
 	keya,keys,keyd,keyf,keyg,keyh,keyj,keyk,keyl,keySEMICOLON,
 	keyz,keyx,keyc,keyv,keyb,keyn,keym,keyCOMMA,keyPERIOD,keyFORWARDSLASH,
-	keyEXCLAMATION=65,keyAT,keyPOUND,keyDOLLAR
+	keyEXCLAMATION=65,keyAT,keyPOUND,keyDOLLAR,keyPERCENT,keyEXPONENT,
+	keyAND,keyASTERISK,keyLEFT_PARENTHESES,keyRIGHT_PARENTHESES,
+	keyQ,keyW,keyE,keyR,keyT,keyY,keyU,keyI,keyO,keyP,
+	keyA,keyS,keyD,keyF,keyG,keyH,keyJ,keyK,keyL,keyCOLON,
+	keyZ,keyX,keyC,keyV,keyB,keyN,keyM,keyLEFTANGLE,keyRIGHTANGLE,keyQUESTION
 };
 
 typedef enum keycode_t keycode_t;
